e9c38c0ecde64c32e8d06ed9a341b87ffe798c32
razik-khan <khanrazik1996@gmail.com>
2020-06-14 23:23:24 +0530

    spinx web design

---
+++ js/hamburger.js
@@ -0,0 +1,37 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+
+
+
+
+
+
+
+
+
+// bottom to top
+
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
